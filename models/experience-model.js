8a7759d8814f71099aa6ae629032331cede36472
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-03-03 10:39:35 +0100

    create experience model

---
+++ models/experience-model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const experienceSchema = new Schema({
+  date: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+  },
+  imageUrl: {
+    type: String,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, {
+  timestamps: true
+});
+
+
+const Experience = mongoose.model('Experience', experienceSchema);
+
+module.exports = Experience;
